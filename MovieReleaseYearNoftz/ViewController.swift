b223c641a3b998a8558c4f053089ef99aca7668a
esnoftz <esnoftz@gmail.com>
2025-01-14 08:08:02 -0600

    Got json object to print.

--- MovieReleaseYearNoftz/ViewController.swift
+++ MovieReleaseYearNoftz/ViewController.swift
@@ -11,9 +11,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getMovie()
     }
+    
+    func getMovie() {
+        let session = URLSession.shared
 
-
+        let movieURL = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=bf06009d")!
+        
+        // must allow arbitrary load
+        let dataTask = session.dataTask(with: movieURL) { data, response, error in
+            if let e = error {
+                print("Error \(e)")
+            } else {
+                if let d = data {
+                    
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as! NSDictionary {
+                        //print("working.")
+                        print(jsonObj)
+                    } else {
+                        print("Error converting to json")
+                    }
+                    
+                } else {
+                    print("Error fetching data")
+                }
+                
+            }
+            
+        }
+        
+        dataTask.resume()
+        
+        
+        
+        
+    }
+    
+    
 }
 
