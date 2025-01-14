876321f2034a0d949df19849f2ddd8983b8089eb
esnoftz <esnoftz@gmail.com>
2025-01-14 07:32:55 -0600

    Initial Commit

---
+++ MovieReleaseYearNoftz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieReleaseYearNoftz
+//
+//  Created by EVANGELINE NOFTZ on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieReleaseYearNoftzTests/MovieReleaseYearNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReleaseYearNoftzTests.swift
+//  MovieReleaseYearNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 1/14/25.
+//
+
+import Testing
+@testable import MovieReleaseYearNoftz
+
+struct MovieReleaseYearNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
