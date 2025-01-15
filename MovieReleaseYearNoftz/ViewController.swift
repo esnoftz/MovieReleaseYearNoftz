5923346baf0c3745436f76bd1e5efb7c89a1dbe7
esnoftz <esnoftz@gmail.com>
2025-01-15 08:06:15 -0600

    Finished assignment.

--- MovieReleaseYearNoftz/ViewController.swift
+++ MovieReleaseYearNoftz/ViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var releaseDateOutlet: UILabel!
+    
+    @IBOutlet weak var movieChoiceTextField: UITextField!
+    
+    var movieName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getMovie()
+        //getMovie()
     }
     
     func getMovie() {
+        //var movieName = "Ghost"
         let session = URLSession.shared
 
-        let movieURL = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=bf06009d")!
+        let movieURL = URL(string: "http://www.omdbapi.com/?t=\(movieName)&apikey=bf06009d")!
         
         // must allow arbitrary load
         let dataTask = session.dataTask(with: movieURL) { data, response, error in
@@ -30,6 +37,22 @@ class ViewController: UIViewController {
                     if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as! NSDictionary {
                         //print("working.")
                         print(jsonObj)
+                        
+                        if let theTitle = jsonObj.value(forKey: "Title") {
+                            if let theYear = jsonObj.value(forKey: "Year") {
+                                // happens on the main thread
+                                DispatchQueue.main.async {
+                                    self.releaseDateOutlet.text = "\(self.movieName) was released in \(theYear)"
+                                }
+                            }
+                            
+                        } else {
+                            DispatchQueue.main.async {
+                                self.releaseDateOutlet.text = "That movie does not exist!"
+                            }
+                            
+                        }
+                        
                     } else {
                         print("Error converting to json")
                     }
@@ -45,8 +68,12 @@ class ViewController: UIViewController {
         dataTask.resume()
         
         
-        
-        
+    }
+    
+    
+    @IBAction func chooseMovieAction(_ sender: UIButton) {
+        self.movieName = movieChoiceTextField.text!
+        getMovie()
     }
     
     
